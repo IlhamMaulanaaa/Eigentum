73afd4e784fb29edf091e624e8fb1952a0978054
Ibrahim Ibtaz <ibrahimbtaz@gmail.com>
2023-04-18 00:32:41 +0700

    SI : add nav

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -16,7 +16,7 @@
 rfrfrfr
 <body class="">
     @include('partial.nav')
-    {{-- @include('partial.footer') --}}
+    @include('partial.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
     </script>
