3df001104a6f7b40522415333372f562a144e270
ilhammaulana <xilhammaulanax739@gmail.com>
2023-04-17 22:52:53 +0700

    [IM] add footer

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>eigentum</title>
+    <link rel="stylesheet" href="/css/footer.css">
+    <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+</head>
+
+<body class="bg-dark">
+    @include('partial.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+
+
+</body>
+
+</html>
---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,110 @@
+<div id="content-container">
+    <div id="_bg__footer"></div>
+    <div id="rectangle_60"></div>
+
+    <div id="footer_ui">
+
+        <div id="group_126">
+            <div id="divider"></div>
+
+            <div id="group_125">
+
+                <div id="social">
+
+                    <a href="https://twitter.com/" target="_blank">
+                        <div id="twitter_icon">
+                            <div id="rectangle"></div>
+                            <img src="/assets/skins/twitter.png" id="twitter" />
+
+                        </div>
+                    </a>
+
+                    <a href="https://www.linkedin.com/" target="_blank">
+                        
+                        <div id="linkedin_icon">
+                            <div id="rectangle_ek1"></div>
+                            
+                            <div id="group_14">
+                                <img src="/assets/skins/shape.png" id="shape" />
+                                <img src="/assets/skins/shape_ek1.png" id="shape_ek1" />
+                                <img src="/assets/skins/shape_ek2.png" id="shape_ek2" />
+                                
+                            </div>
+                            
+                        </div>
+                    </a>
+                    
+                    <a href="https://www.linkedin.com/" target="_blank">
+                    <div id="facebook_icon">
+                        <div id="rectangle_ek2"></div>
+                        <img src="/assets/skins/shape_ek3.png" id="shape_ek3" />
+
+                    </div>
+                    </a>
+
+                </div>
+                <div id="___2019_apcom__all_ri">
+                    <span class="char">%C2%A9 </span> 2023 Eigentum. All Rights Reserved.
+                </div>
+                <div id="eigentum">
+                    Eigentum
+                </div>
+
+            </div>
+
+            <div id="group_124">
+
+                <div id="talk_to_us">
+                    <div id="support_apcom_com__6">
+                        <a href="mailto:eigentumofficial@gmail.com" class="linkeigentum"
+                            target="_blank">eigentumofficial@gmail.com</a><br />+62 896-2090-2267<br />Contact
+                        Us<br />Facebook<br />Linkedin<br />Twitter
+                    </div>
+                    <div id="talk_to_us_ek1">
+                        TALK TO US
+                    </div>
+
+                </div>
+
+                <div id="navigation">
+                    <div id="home_about_us_what_w">
+                        Perumahan <br />Apartemen <br />Villa
+                    </div>
+                    <div id="navigation_ek1">
+                        Produk
+                    </div>
+
+                </div>
+
+                <div id="legal">
+                    <div id="general_info_privacy">
+                        General Info<br />Privacy Policy<br />Terms of Service
+                    </div>
+                    <div id="legal_ek1">
+                        LEGAL
+                    </div>
+
+                </div>
+
+                <div id="what_we_do">
+                    <div id="encouraging_testing">
+                        <span style="font-style:normal; font-weight:normal; text-decoration:NONE; ">menyediakan
+                            informasi yang akurat<br />memudahkan calon pembeli<br />mempromosikan
+                            properti<br />menyediakan layanan konsultasi<br /></span><span
+                            style="font-style:normal; font-weight:normal; text-decoration:NONE; ">Engaging With
+                            Global Fund<br />Shining a Ligh</span>
+                    </div>
+                    <div id="what_we_do_ek1">
+                        What We Do
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<script>var specialChars = document.querySelectorAll("span.char"); for (var c = 0; c < specialChars.length; c++) { specialChars[c].innerHTML = decodeURIComponent(specialChars[c].innerHTML); }</script>
