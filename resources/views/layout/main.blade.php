bccc6d0e1601b77473b938fa54540f76213ac5ee
MChassanNajib <chassannajib@gmail.com>
2023-05-18 14:54:28 +0700

    feat [CN] : Create Responnsive Navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,21 +6,83 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>eigentum</title>
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/nav.css">
-    <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
-</head>
-<body class="">
-    @include('partial.nav')
-    @include('partial.footer')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-    </script>
+    <link rel="stylesheet" href="/css/Home/home.css">
+    <link rel="stylesheet" href="/css/swiper-bundle.min.css">
+    <link
+      href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/Lib/swiper-bundle.min.css">
 
+    <!-- Icon -->
+    <link
+      href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+      rel="stylesheet"/>
+    <script src="https://kit.fontawesome.com/your_kit_code.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-...your_integrity_code..." crossorigin="anonymous" />
+  
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/css/Lib/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="/css/Dijual/dijual.css">
+    <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="/css/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="/css/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="/css/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="/css/Lib/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="/css/Panduan/panduan.css" rel="stylesheet">
+    
+    <link href="/css/Panduan/panduan.css" rel="stylesheet">
+</head>
+
+<body>
+        @if (Auth::check())
+            @include('partial.nav')
+        @endif
+            
+            {{-- @include('partial.nav')
+            @include('partial.footer') --}}
+            
+            @yield('content')
+            {{-- @include('partial.footer') --}}
+
+
+    {{-- <div class="container py-1">
+        @if (Auth::check())
+        @endif
+        @include('partial.nav')
+        <div class="container">
+            @yield('container')
+        </div>
+        @include('page.Home.home')
+    </div> --}}
 
 </body>
 
+
+<script src="/js/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    </script>
+<!-- JavaScript -->
+<script src="/js/home.js"></script>
 </html>
