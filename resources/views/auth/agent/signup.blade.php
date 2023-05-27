1e68236c3a85bdd088bed296a0687b1af38c8977
ilhammaulana <xilhammaulanax739@gmail.com>
2023-05-27 11:42:40 +0700

    IM: feat register/login agent

--- resources/views/auth/agent/signup.blade.php
+++ resources/views/auth/agent/signup.blade.php
@@ -10,7 +10,7 @@
     <!-- Tambahkan link ini untuk menghubungkan library Font Awesome -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/Login and Register/agen/login.css">
+    <link rel="stylesheet" href="/css/Login and Register/agen/register.css">
 </head>
 
 
@@ -20,86 +20,108 @@
             <div class="row">
                 <div class="col-sm-6 text-black">
                     <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                        <img class="mb-4" src="/assets/login/logo_1.png" alt="">
-
-                        <span class="h1 fw-regular mb-0" style="color: #0c40e8;">Eigentum
-                        </span>
                         <form action="/session/signup/create" method="POST">
                             @csrf
-                            <h1 class="h1 fw-normal text-dark" id="masuk009">Memulai</h1>
-                            <h6 class="h6 fw-normal text-dark" id="masuk026">Sudah punya akun? <a
-                                    href="/session/signin" class="text-dark " id="adkjslf">Sign
-                                    In</a></h6>
-                            <div class="form-floating">
-                                <input type="text" value="{{ Session::get('name') }}" name="name"
-                                    class="form-control" id="name" placeholder="Username">
-                                <label for="name">Username</label>
+                            <div class="logo">
+                                <img class="mb-4 image_logo" src="/assets/login/logo_1.png" alt="">
+                                <span class="h3 text_logo fw-regular " style="color: #0c40e8;">Eigentum
+                                </span>
                             </div>
                             <br>
-                            <div class="form-floating">
-                                <input type="email" value="{{ Session::get('email') }}" name="email"
-                                    class="form-control" id="email" placeholder="name@example.com">
-                                <label for="email">Email address</label>
+                            <div class="row">
+                                <div class="column">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" placeholder="email">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password"
+                                            placeholder="Password">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="namaLengkap">Nama Lengkap</label>
+                                        <input type="text" class="form-control" id="namaLengkap"
+                                            placeholder="Nama Lengkap">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="nomerHp">Nomer Handphone</label>
+                                        <input type="number" class="form-control" id="nomerHp"
+                                            placeholder="Nomer Handphone">
+                                    </div>
+                                    <br>
+                                    <div class="mb-3 mt-4">
+                                        <label for="exampleFormControlTextarea1" class="form-label">Alamat</label>
+                                        <textarea class="form-control" id="alamat" rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <div class="column"></div>
                             </div>
-                            <br>
-                            <div class="form-floating">
-                                <input type="password" class="form-control" id="password" name="password"
-                                    placeholder="Password">
-                                <label for="password">Password</label>
-                            </div>
-
-
-                            <button class="w-100 btn btn-lg btn-primary" id="loginbutton" type="submit">Sign
-                                Up</button>
-                            <button class="w-100 mt-2 btn btn-lg btn-primary" id="jamal009" type="submit">
-                                <img id="googleicon" src="/assets/login/google.png" alt="">
-                                Lanjutkan dengan google
-                            </button>
-                            <pre><p id="privacy_police">you agree to be bound by these <u> Terms of Service</u> and our
-<u>Privacy Policy.</u></p></pre>
                         </form>
-
+                    </div>
+                </div>
+                <div class="col-sm-6 column_2">
+                    <br>
+                    <label for="exampleFormControlTextarea1" class="label lokasi">Lokasi</label>
+                    <select class="form-select drp1" aria-label="Default select example">
+                        <option selected>provinsi</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <select class="form-select drp2" aria-label="Default select example">
+                        <option selected>kota</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <select class="form-select drp3" aria-label="Default select example">
+                        <option selected>distrik</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <div class="row nte mt-5" style="margin:auto;">
+                        <span class="border border-dark col-md-8 rounded" id="border_upload_ktp"
+                            style="display:inline-block; height: 150px; width: 150px;">
+                            <i class="fa fa-upload" aria-hidden="false"></i>
+                        </span>
+                        <div class="col-md-4 nce">
+                            <h5>Upload foto ktp</h5>
+                            <p class="upload_foto row mb-3">Pastikan foto-foto yang diunggah adalah foto asli dan
+                                memiliki
+                                hak cipta yang sah. Jangan mengunggah foto yang melanggar hak cipta atau foto yang
+                                diambil dari sumber yang tidak diizinkan.</p>
+                            <button type="button" class="btn button_upload1">Upload</button>
+                        </div>
+                    </div>
+                    <div class="row nte mt-3 " style="margin:auto;">
+                        <span class="border border-dark col-md-8 rounded " id="border_upload_ktp"
+                            style="display:inline-block; height: 150px; width: 150px;">
+                            <i class="fa fa-upload" aria-hidden="false"></i>
+                        </span>
+                        <div class="col-md-4 nce">
+                            <h5>Upload foto selfie anda bersama ktp</h5>
+                            <p class="upload_foto row mb-3">Pastikan foto-foto yang diunggah adalah foto asli dan
+                                memiliki
+                                hak cipta yang sah. Jangan mengunggah foto yang melanggar hak cipta atau foto yang
+                                diambil dari sumber yang tidak diizinkan.</p>
+                            <button type="button" class="btn button_upload1">Upload</button>
+                        </div>
                     </div>
 
-                </div>
-                <div class=" col-sm-6 px-0 d-flex d-sm-block">
-                    <img id="image1009" src="/assets/login/Component 169.svg" alt="image 1">
+
                 </div>
             </div>
         </div>
+        <div class="register">
+            <button type="button" class="btn button_register">Register</button>
+        </div>
     </section>
-    <script>
-        // Daftar sumber gambar
-        var daftarGambar = [
-            "/assets/login/Component 169.svg",
-            "/assets/login/Component 170.svg",
-            "/assets/login/Component 171.svg",
-            "/assets/login/Component 172.svg",
-        ];
 
-        // Mendapatkan elemen gambar
-        var gambar = document.getElementById("image1009");
-
-        // Mengatur indeks awal gambar
-        var indeksGambar = 0;
-
-        // Fungsi untuk mengganti gambar
-        function gantiGambar() {
-            // Mengatur sumber gambar baru
-            gambar.src = daftarGambar[indeksGambar];
-
-            // Menambahkan 1 ke indeks gambar
-            indeksGambar++;
-
-            // Jika indeks gambar melebihi jumlah gambar, kembali ke indeks awal
-            if (indeksGambar === daftarGambar.length) {
-                indeksGambar = 0;
-            }
-        }
-
-        // Memanggil fungsi gantiGambar setiap 2 detik
-        setInterval(gantiGambar, 2000);
-    </script>
 </body>
 
 </html>
